1e89559c9d8c57b8a82f9e89dfa84f0a9d8f8832
absoluteAquarian <jruss88@me.com>
2025-02-19 22:38:12 -0600

    First task completed: renaming via refactoring

--- 02-pycharm-power-user/main.py
+++ 02-pycharm-power-user/main.py
@@ -1,47 +1,47 @@
-def p(b):
-    for r in b:
-        print(" | ".join(r))
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
         print("-" * 5)
 
 
-def c_w(b, p):
+def has_player_won(board, player):
     for i in range(3):
-        if all(b[i][j] == p for j in range(3)) or all(b[j][i] == p for j in range(3)):
+        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
             return True
-    if all(b[i][i] == p for i in range(3)) or all(b[i][2 - i] == p for i in range(3)):
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
         return True
     return False
 
 
-def f(b):
+def is_board_full(b):
     return all(c != " " for r in b for c in r)
 
 
-def t():
-    b = [[" " for _ in range(3)] for _ in range(3)]
-    p = ["X", "O"]
+def play_the_game():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
     print("Tic-Tac-Toe Game")
-    p(b)
-    for t in range(9):
-        pl = p[t % 2]
+    print_board(board)
+    for turn in range(9):
+        player = players[turn % 2]
         while 1:
             try:
-                r, c = map(int, input(f"P {pl}, row col (0-2): ").split())
-                if b[r][c] == " ":
-                    b[r][c] = pl
+                row, column = map(int, input(f"P {player}, row col (0-2): ").split())
+                if board[row][column] == " ":
+                    board[row][column] = player
                     break
                 else:
                     print("Nope. Again.")
             except:
                 print("Wrong. 0-2 pls.")
-        p(b)
-        if c_w(b, pl):
-            print(f"P {pl} wins!")
+        print_board(board)
+        if has_player_won(board, player):
+            print(f"P {player} wins!")
             return
-        if f(b):
+        if is_board_full(board):
             print("Draw!")
             return
     print("Draw!")
 
 
-t() 
+play_the_game()
