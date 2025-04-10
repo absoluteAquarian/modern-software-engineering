18b314bd811bf392cf2b00a8e291938ad747e000
absoluteAquarian <jruss88@me.com>
2025-04-10 01:04:09 -0500

    Implement fixes to pass pylint, flake8 and mypy

--- 05-code-quality/main.py
+++ 05-code-quality/main.py
@@ -1,18 +1,36 @@
-import math
-import random
+"""
+A simple Python program
+"""
 
-def expensive_op(n):
-    return n * 999 * 1000 // 2
+
+def expensive_op(number):
+    """
+    This function performs arithmetic on a number
+    :param number: The number
+    :return: A value derived from the input
+    """
+    return number * 999 * 1000 // 2
+
 
 def slow_func(lst):
+    """
+    This function generates a new lines based on the contents of an input list
+    :param lst: The input list
+    :return: A new list derived from the input
+    """
     result = []
     for i in range(len(lst)):
         result.append(expensive_op(i))
     return result
 
+
 def main():
+    """
+    A basic main function
+    :return: None
+    """
     numbers = list(range(1000))
-    output = slow_func(numbers)
+    slow_func(numbers)
     print("Done")
 
 
